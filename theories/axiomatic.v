062d79ceb8322a82f8002543323051a6887a7f06
jp <jp622@cam.ac.uk>
2021-06-16 13:36:37 +0100

    remove dependencies of axiomatic def from rest of development

--- theories/axiomatic.v
+++ theories/axiomatic.v
@@ -1,9 +1,8 @@
-Require Import isla.base isla.opsem.
+Require Import Coq.Relations.Relation_Operators Coq.Classes.RelationClasses.
+Require Import BinNat.
 
-From RecordUpdate Require Import RecordSet.
-Import RecordSetNotations.
-
-Require Import Coq.Relations.Relation_Operators.
+(* TODO: so we don't have dependencies *)
+Axiom bv : N -> Type.
 
 Definition thread_id := N.
 Definition event_id := N.
@@ -67,6 +66,10 @@ Inductive asid_or_global :=
 | AG_asid (asid:asid)
 | AG_global.
 
+
+Definition addr := bv 64.
+Definition byte := bv 8.
+
 Record write_address := {
   wa_kind : write_kind;
   wa_input_address : addr;
@@ -125,7 +128,7 @@ Record barrier_info := {
  b_kind : barrier_kind;
 }.
 
-Inductive memory_action : Set :=
+Inductive memory_action : Type :=
 | MA_write_announce (x:write_info)
 | MA_write (x:write_info)
 | MA_read (x:read_info)
@@ -142,17 +145,6 @@ Record event_info := {
   ei_label: memory_action;
   ei_inst: instruction;
 }.
-
-(*
-Inductive memory_action : Type :=
-| MA_act (a:trace_label)
-  (* TODO: we use these for now, but
-  (1) they have to change, e.g. they don't currently have barriers, translate vs. read distinction, faults, etc., and
-  (2) the interface between isla-coq and the memory model itself feels wrong
-  *)
-| MA_fetch (a:addr) (v:valu)
-| MA_translate (stg:translation_stage) (hi:addr) (lo:valu).
-*)
 
 Definition rel := event_id -> event_id -> Prop.
 
@@ -176,33 +168,6 @@ Record pre_execution := {
 
 Definition is_not_target (r : rel) (e : event_id) : Prop :=
   forall e', ~ r e' e.
-
-Definition addr_of_aux (v:valu) : option addr :=
-  match v with
-  | Val_Bits (BVN 64 x) => Some x
-  | _ => None
-  end.
-
-Definition addr_of (a:trace_label) : option addr :=
-  match a with
-  | LReadMem _ _ addr _ _ => addr_of_aux addr
-  | LWriteMem _ _  addr _ _ _ => addr_of_aux addr
-  | _ => None
-  end.
-
-(* TODO: ??? *)
-Definition read_kind_of_valu (v : valu) : read_kind :=
-  RK_plain.
-
-(* TODO: ??? *)
-Definition write_kind_of_valu (v : valu) : write_kind :=
-  WK_plain.
-
-Definition tag_value_of_valu_option (vo : valu_option) :=
-  match vo with
-  | None => None
-  | Some x => Some true (* TODO: this is wrong! *)
-  end.
 
 Record memory_model := {
   mm_validity : pre_execution -> Prop;
@@ -250,7 +215,7 @@ Definition wf_fpo (pe : pre_execution) (fpo : rel) : Prop :=
   Transitive fpo /\
   Irreflexive fpo /\
   (forall e e', tid_of pe e <> 0%N ->
-    is_fetch pe e -> is_fetch pe e' ->
+    is_fetch pe e = true -> is_fetch pe e' = true ->
     tid_of pe e = tid_of pe e' -> either_way fpo e e') /\
   (forall e e', fpo e e' -> tid_of pe e = tid_of pe e').
 
@@ -264,7 +229,7 @@ Definition wf_iio (pe : pre_execution) (iio : rel) : Prop :=
 
 Definition wf_tdata (pe : pre_execution) (tdata : rel) : Prop :=
   does_not_involve_initial_events pe tdata /\
-  (forall e e', tdata e e' -> tid_of pe e = tid_of pe e' /\ is_translate pe e').
+  (forall e e', tdata e e' -> tid_of pe e = tid_of pe e' /\ is_translate pe e' = true).
 
 Definition wf_pre_execution (pe : pre_execution) : Prop :=
   (forall e, (tid_of pe e <= pe.(pe_threads))%N) /\
@@ -332,7 +297,7 @@ Definition sc : memory_model := {|
   wf_irf pe irf /\
   wf_co pe co /\
   let fr := (fun r w => exists w0, rf w0 r /\ co w0 w) in
-  let fe := (fun e1 e2 => pe.(pe_iio) e1 e2 /\ is_fetch pe e1 /\ is_explicit_event pe e2) in
+  let fe := (fun e1 e2 => pe.(pe_iio) e1 e2 /\ is_fetch pe e1 = true /\ is_explicit_event pe e2 = true) in
   let po := (fun a b => exists fa fb, fe fa a /\ fe fb b /\ pe.(pe_fpo) fa fb) in
   let po_fetch0 := (fun a fb => exists b, po a b /\ fe fb b) in
   let po_fetch := (fun a b => po_fetch0 a b \/ fe a b) in
