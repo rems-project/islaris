b76dad8c15204e68296c1415b0271ae973105376
Michael Sammler <noreply@sammler.me>
2021-09-16 13:25:05 +0200

    don't give hypothesis for reading from reg_col

--- theories/automation.v
+++ theories/automation.v
@@ -400,18 +400,17 @@ Section instances.
     (find_in_context (FindRegMapsTo r) (λ rk,
       match rk with
       | RKMapsTo v' => (⌜v = v'⌝ -∗ r ↦ᵣ v' -∗ WPasm es)
-      | RKCol regs => ∃ i, ⌜(via_vm_compute (list_find_idx (λ x, x.1 = RegColDirect r)) regs) = Some i⌝ ∗
-                      (∀ vr, ⌜regs !! i = Some vr⌝ -∗ ⌜if vr.2 is Some v' then v = v' else True⌝ -∗
-                       reg_col regs -∗ WPasm es)
+      | RKCol regs => ⌜is_Some (via_vm_compute (list_find_idx (λ x, x.1 = RegColDirect r)) regs)⌝ ∗
+                      (reg_col regs -∗ WPasm es)
       end)) -∗
     WPasm (ReadReg r [] v ann :: es).
   Proof.
     iDestruct 1 as (rk) "[Hr Hwp]" => /=. case_match; simplify_eq.
     - by iApply (wp_read_reg with "Hr").
     - rewrite via_vm_compute_eq.
-      iDestruct "Hwp" as (i [[??][?[??]]]%list_find_idx_Some) "Hwp"; simplify_eq/=.
+      iDestruct "Hwp" as ([? [[??][?[??]]]%list_find_idx_Some]) "Hwp"; simplify_eq/=.
       iDestruct (big_sepL_lookup_acc with "Hr") as "[[%vact [% Hr]] Hregs]"; [done|] => /=.
-      iApply (wp_read_reg with "Hr"). iIntros "% Hr". subst. iApply "Hwp"; [done..|]. iApply "Hregs".
+      iApply (wp_read_reg with "Hr"). iIntros "% Hr". iApply "Hwp". iApply "Hregs".
       iExists _. by iFrame.
   Qed.
 
@@ -420,19 +419,18 @@ Section instances.
      (find_in_context (FindStructRegMapsTo r f) (λ rk,
       match rk with
       | RKMapsTo v' => (⌜vread = v'⌝ -∗ r # f ↦ᵣ v' -∗ WPasm es)
-      | RKCol regs => ∃ i, ⌜(via_vm_compute (list_find_idx (λ x, x.1 = RegColStruct r f)) regs) = Some i⌝ ∗
-                      (∀ vr, ⌜regs !! i = Some vr⌝ -∗ ⌜if vr.2 is Some v' then vread = v' else True⌝ -∗
-                      reg_col regs -∗ WPasm es)
+      | RKCol regs => ⌜is_Some (via_vm_compute (list_find_idx (λ x, x.1 = RegColStruct r f)) regs)⌝ ∗
+                      (reg_col regs -∗ WPasm es)
       end))) -∗
     WPasm (ReadReg r [Field f] v ann :: es).
   Proof.
     iDestruct 1 as (???) "[Hr Hwp]" => /=. case_match; simplify_eq.
     - by iApply (wp_read_reg_struct with "Hr").
     - rewrite via_vm_compute_eq.
-      iDestruct "Hwp" as (i [[??][?[??]]]%list_find_idx_Some) "Hwp"; simplify_eq/=.
+      iDestruct "Hwp" as ([? [[??][?[??]]]%list_find_idx_Some]) "Hwp"; simplify_eq/=.
       iDestruct (big_sepL_lookup_acc with "Hr") as "[[%vact [% Hr]] Hregs]"; [done|] => /=.
-      iApply (wp_read_reg_struct with "Hr"); [done|]. iIntros "% Hr". subst.
-      iApply "Hwp"; [done..|]. iApply "Hregs". iExists _. by iFrame.
+      iApply (wp_read_reg_struct with "Hr"); [done|]. iIntros "% Hr". iApply "Hwp". iApply "Hregs".
+      iExists _. by iFrame.
   Qed.
 
   Lemma li_wp_write_reg r v ann es:
