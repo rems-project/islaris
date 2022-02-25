94152b10537591b6cc1268846ec364a3637917b1
Michael Sammler <noreply@sammler.me>
2022-02-25 16:53:29 +0100

    work around recursive calls in coqc_timing.sh

--- bin/coqc_timing.sh
+++ bin/coqc_timing.sh
@@ -5,9 +5,10 @@ set -e
 # Wrapper for coqc that is used when running the perf script in the CI.
 # Variable TIMECMD is expected to contain an absolute path to the perf script.
 # If TIMECMD is not set (or empty), fallback to just calling coqc.
+# we need to use opam exec -- coqc to get the coqc installed by opam, not this script
 
 if [ -z "${TIMECMD}" ]; then
-  coqc "$@"
+  opam exec -- coqc "$@"
 else
-  ${TIMECMD} coqc "$@"
+  ${TIMECMD} opam exec -- coqc "$@"
 fi
