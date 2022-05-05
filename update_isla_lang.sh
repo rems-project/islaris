6bdf437a8b4866f25b5a9de1522ae17f9ba82459
Brian Campbell <Brian.Campbell@ed.ac.uk>
2022-05-05 22:49:34 +0100

    Update isla-lang dependency Fixes #16

--- update_isla_lang.sh
+++ update_isla_lang.sh
@@ -4,7 +4,7 @@
 # the repository. Note that the script is self-modifying: it will change the
 # old hash into the new one, and erase the new hash again.
 
-OLD_HASH=ea66fcb67b541339cd8139ec110be96dd00758d5
+OLD_HASH=413f1d1c7fa725d757662d044dcebbf9d2e57d2e
 NEW_HASH=
 
 sed -i "s/${OLD_HASH}/${NEW_HASH}/g" README.md .gitlab-ci.yml .github/workflows/ci.yml update_isla_lang.sh
